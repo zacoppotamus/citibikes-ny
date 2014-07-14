a23791e0c49c79e8edb33e66038359875257e45f
Zac Ioannidis <zacoppotamus@gmail.com>
2014-07-14 02:20:22 -0400

    Adding API routes

--- server/api/trip/index.js
+++ server/api/trip/index.js
@@ -6,12 +6,10 @@ var controller = require('./trip.controller');
 var router = express.Router();
 
 // Only need GET for accessing the DB.
-router.get('/getStations/all', controller.getStations);
-router.get('/:id', controller.show);
-// router.get('/:blabla', controller.greet);
-// router.post('/', controller.create);
-// router.put('/:id', controller.update);
-// router.patch('/:id', controller.update);
-// router.delete('/:id', controller.destroy);
+router.get('/getStations', controller.getStations);
+router.get('/top_dest/:sname&limit=:limit', controller.topDest);
+router.get('/top_dest/:sname', controller.topDest);
+
+router.get('/', controller.all);
 
 module.exports = router;
--- server/api/trip/trip.controller.js
+++ server/api/trip/trip.controller.js
@@ -1,8 +1,9 @@
 /**
  * Using Rails-like standard naming convention for endpoints.
- * GET     /trips              ->  index
- * GET     /trips/:id          ->  show
- * GET     /trips/getStations  ->  getStations
+ * GET     /getStations     ->  getStations -> (get all station names)
+ * GET     /top_dest/:sname&limit=:limit?      ->  get top N destinations
+ *         (defaults to 5)
+ * GET     /    ->  all
  */
 
 'use strict';
@@ -10,38 +11,45 @@
 var _ = require('lodash');
 var Trip = require('./trip.model');
 
-// Get list of trips
-// exports.index = function(req, res) {
-//   console.log(Trip.findOne());
-//   Trip.find(function (err, trips) {
-//     if(err) { return handleError(res, err); }
-//     return res.json(200, trips);
-//   });
-// };
-
-// Greet
-// exports.greet = function(req, res) {
-//     return res.json('hi');
-// }
 
 // Get a single trip
-exports.show = function(req, res) {
-  console.log(Trip.findOne());
-  Trip.findById(req.params.id, function (err, trip) {
-    if(err) { return handleError(res, err); }
-    if(!trip) { return res.send(404); }
-    return res.json(trip);
-  });
+exports.all = function(req, res) {
+    var fields = _.keys(req.query).join(' ');
+
+    console.log(req.query);
+
+    Trip.find(req.query, function (err, trip) {
+        if(err) { return handleError(res, err); }
+        if(!trip) { return res.send(404); }
+        return res.json(trip);
+    });
+    
 };
 
 exports.getStations = function(req, res) {
-    console.log(req);
-    Trip.distinct('start station name', function(err, trip) { 
+    Trip.distinct('start_sname', function(err, station) { 
         if(err) { return handleError(res,err); }
-        return res.json(trip);
+        return res.json(station.sort());
     });
 };
 
+exports.topDest = function(req, res) {
+    // console.log(req.queries.sname);
+    // console.log(req.queries.limit);
+    var queryLimit = req.params.limit || 5; // limit defaults to 5
+    console.log('queryLimit is ' + queryLimit);
+    Trip.aggregate(
+        [
+            {"$match" : {"start_sname" : "E 3 St & 1 Ave"}},
+            {"$group" : {"_id" : "$end_sname", "sum" : {"$sum":1}}},
+            {"$sort" : {"sum" : -1}},
+            {"$limit" : 10}
+        ], function(err, results) {
+            return res.json(results);
+    });
+};
+
+
 function handleError(res, err) {
-  return res.send(500, err);
+    return res.send(500, err);
 }
